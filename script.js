29f2654d379370c65f323c0873f7e10d1bafb89c
Lily <2475165+TsukihanaNo@users.noreply.github.com>
2023-07-30 18:57:06 -0400

    functional

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let currentText = ""
 let previousText = ""
-let operator = ""
+const numArray = []
+const operatorArray = ["+"]
 
 const nums = document.querySelectorAll(".num");
 nums.forEach(num => num.addEventListener("click",addNum))
@@ -11,15 +12,80 @@ operators.forEach(operator => operator.addEventListener("click",addOperator))
 const topLine = document.querySelector(".top-line");
 const bottomLine = document.querySelector(".bottom-line")
 
+const buttonClear = document.querySelector(".clear")
+buttonClear.addEventListener("click",clear)
+
+const buttonClearAll = document.querySelector(".clear-all")
+buttonClearAll.addEventListener("click",clearAll)
+
+function clear (e){
+    currentText = "0"
+    bottomLine.textContent=currentText
+}
+
+function clearAll (e){
+    currentText= "0"
+    bottomLine.textContent = currentText
+    topLine.textContent = ""
+    numArray.length = 0
+    operatorArray.length = 0
+    operatorArray.push("+")
+}
+
 function addNum(e){
-    currentText+=e.target.getAttribute("value");
+    if (bottomLine.textContent=="0"){
+        currentText=e.target.getAttribute("value");
+    }else{
+        currentText+=e.target.getAttribute("value");
+    }
     bottomLine.textContent=currentText;
 }
 
 function addOperator (e){
     operator = e.target.getAttribute("value");
-    previousText = previousText+currentText + operator;
-    currentText="";
-    topLine.textContent=previousText;
-    bottomLine.textContent=currentText;
+
+    if (operator != "="){
+        console.log(operatorArray)
+        if (operatorArray.includes("=")){
+            previousText = bottomLine.textContent + operator;
+            number= parseInt(bottomLine.textContent)
+            numArray.length = 0
+            operatorArray.length = 0
+            operatorArray.push("+")
+        }else{
+            previousText = previousText+currentText + operator;
+            number= parseInt(currentText);
+        }
+        numArray.push(number)
+        operatorArray.push(operator)
+        topLine.textContent=previousText;
+        currentText="0";
+        bottomLine.textContent=currentText;
+    }else{
+        number= parseInt(currentText);
+        numArray.push(number)
+        operatorArray.push(operator)
+        previousText = previousText+currentText;
+        topLine.textContent=previousText;
+        let result = 0
+        for (let i = 0; i<numArray.length;i++){
+            switch (operatorArray[i]){
+                case "+":
+                    result += numArray[i];
+                    break;
+                case "-":
+                    result -= numArray[i];
+                    break;
+                case "*":
+                    result *= numArray[i];
+                    break;
+                case "/":
+                    result /= numArray[i];
+                    break;
+            }
+        }
+        bottomLine.textContent = result;
+    }
+    
+    
 }
